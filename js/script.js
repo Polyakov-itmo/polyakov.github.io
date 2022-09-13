0fc883ef3bbd4a2934509271b119eea2e1e19fd9
Spraymay <MaximPolyakovv@yandex.ru>
2022-09-13 22:55:22 +0300

    fix styles and add js menu

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let menuOpened = false
+let menu = document.querySelector('.menu__list')
+document.querySelector('.menu__burger').addEventListener('click', () => {
+  if (menuOpened) {
+    menu.style.display = 'none'
+    menuOpened = false
+  } else {
+    menu.style.display = 'flex'
+    menuOpened = true
+  }
+})
